a412c466a8c710ad905f19c368084477adfba6e0
hackerter <hackerter@gmail.com>
2017-06-19 00:01:04 -0400

    Comenzando con el código

--- CuboXCubo.py
+++ CuboXCubo.py
@@ -1,1 +1,12 @@
- 
+#Para este ejercicio usamos una librería lamada "numpy", para Kubuntu 14,04, con python 2.7.6
+#apt-get install python-numpy
+
+import numpy as lr
+
+def m_cuadrada(xy,z):
+  prl = [[["" for x in xrange(xy)] for x in xrange(xy)] for x in xrange(z)]
+  #condiciones iniciales de las 2 caras cuadradas
+  for 
+  
+  
+  
