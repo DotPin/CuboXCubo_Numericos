ecbd112556d38dce7cfdfb01f7c9d46b14b5ca79
hackerter <hackerter@gmail.com>
2017-06-24 23:38:30 -0400

    Cambios menores

--- CuboXCubo.py
+++ CuboXCubo.py
@@ -9,20 +9,30 @@ def m_cuadrada(xy,z):
   #condiciones iniciales de las 2 caras cuadradas
   #xx será lado caras (xx, xx) "cuadrada"
   #z será profundidad del paralelepípedo
-  #La matriz creada será de la Forma prl[z][xx][xx]
-  h = 0.000001	#condicion de temperaturas
+  #La matriz creada será de la Forma prl[z][xx][xx] con los siguientes valores en los bordes externos de la matriz
+  h = 0.999999999999	#condicion de temperaturas
   R = 12	#temperatura refrigerante
   A = h*(0-r)	#condicion Cara Aislante
   C = h*(60-r)	#condicion 4
   
+  dx2=
+  dy2=
+  dz2=
+  
+  def iz_up(x,y,dd):	# x=nodo anterior	y=contorno	dd=diferencia
+  return (x-2*dd*h*(x-y))
+
   
   
-  for k in range (0,z):
-    for i in range(0,xx):
-      for j in range(0,xx):
+  
+  
+  
+  for k in range (0,z):		#capa
+    for i in range(0,xx):	#largo
+      for j in range(0,xx):	#ancho
 	if k==0:
 	  #aislante
-	  if i=0 or i==xx-1 or j=0 or j == xx-1:
+	  if (i>0 and i<xx-1) and (j==0 or j<xx-1):
 	    prl[k][i][j] = R	#Borde cara
 	  elif i>0 or i<xx-1 or j>0 or j<xx-1:
 	    prl[k][i][j] = A	#Interior cara
